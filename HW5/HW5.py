cc25bc68dd78e8fad66105f14be6858cf554635a
tl32rodan <tl32rodan.cs09g@nctu.edu.tw>
2021-06-21 03:43:13 +0800

    Finish

--- HW5/HW5.py
+++ HW5/HW5.py
@@ -1,21 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import os
 import numpy as np
 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
-from sklearn.metrics import accuracy_score
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as trans
 
+from torch.utils import data
+from sklearn.metrics import accuracy_score
+from resnet import ResNet50
+from densenet import DenseNet121
+
+
+# ## Download dataset 
+# https://drive.google.com/drive/u/3/folders/1sHh6NvuKX6RB5OytLwf4kaqfQ9svJNDQ
 
 # ## Load data
 
-# In[5]:
+# In[3]:
 
 
 x_train = np.load("x_train.npy")
@@ -28,7 +30,7 @@ print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
 
-# In[6]:
+# In[4]:
 
 
 # It's a multi-class classification problem 
@@ -41,50 +43,202 @@ print(np.unique(y_train))
 
 # ## Data preprocess
 
-# In[7]:
+# In[46]:
 
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+class MyCIFARDataset(data.Dataset):
+    def __init__(self, images, labels=None, transform=None):
+        if not (labels is None):
+            assert len(images) == len(labels)
+        
+        self.images = images
+        if transform is None:
+            self.trans = trans.ToTensor()
+        else:
+            self.trans = transform
+        
+        self.labels = labels
+        
+        
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, index):
+        img = self.images[index]
+        img = self.trans(img)
+        if not (self.labels is None):
+            return img, self.labels[index][0]
+        else:
+            return img
 
-# Convert class vectors to one-hot encoding (keras model requires one-hot label as inputs)
-num_classes = 10
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+
+# ## Build model & training
+
+# In[57]:
 
 
-# ## Build model & training (Keras)
+class MyModel:
+    def __init__(self, net):
+        self.net = net.cuda()
 
-# In[9]:
+        # Initiate SGD optimizer
+        self.opt = torch.optim.SGD(
+            net.parameters(),
+            lr=0.1,
+            momentum=0.9,
+            weight_decay=5e-4
+        )
+        #self.lr_sch = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        #    self.opt,
+        #    factor=0.1,
+        #    patience=50,
+        #    verbose=True
+        #)
+        self.lr_sch = torch.optim.lr_scheduler.MultiStepLR(
+            self.opt,
+            milestones=[150, 250, 350], 
+            gamma=0.1
+        )
 
 
-import warnings
-warnings.filterwarnings('ignore')
+        # Use cross-entropy as loss function
+        self.criterion = nn.CrossEntropyLoss().cuda()
+
+        self.train_trans = trans.Compose([
+                trans.ToPILImage(),
+                trans.RandomCrop(32, padding=4),
+                trans.RandomHorizontalFlip(),
+                trans.RandomRotation(degrees=15),
+                trans.ToTensor(),
+                trans.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ])
+
+        self.test_trans = trans.Compose([
+                trans.ToPILImage(),
+                trans.ToTensor(),
+                trans.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ])
+
+
+    def fit(
+        self, x_train, y_train,
+        batch_size=16,
+        epochs=100,
+        validation_data=None,
+        shuffle=True,
+        store_model=True
+    ):
+        # Data preprocessing
+        self.train_set = MyCIFARDataset(x_train, y_train, self.train_trans)
+        self.train_loader = data.DataLoader(
+            dataset=self.train_set,
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=True,
+            num_workers=4
+        )
+
+        self.use_val = not (validation_data is None)
+        if self.use_val:
+            self.val_set = MyCIFARDataset(validation_data[0], validation_data[1], self.test_trans)
+            self.val_loader = data.DataLoader(
+                dataset=self.val_set,
+                batch_size=16,
+                shuffle=False,
+                drop_last=False,
+                num_workers=4
+            )
+        
+        best_acc = 0.
+        # Start training
+        for ep in range(epochs):
+            self.net.train()
+            # Train
+            avg_loss = 0.
+            for input, label in self.train_loader:
+                input, label = input.cuda(), label.cuda()
+                output = self.net(input)
+                loss = self.criterion(output, label)
+                self.opt.zero_grad()
+                loss.backward()
+                self.opt.step()
+                
+                avg_loss += loss.item()
+                
+            avg_loss /= len(self.train_loader)
+            
+            #self.lr_sch.step(avg_loss)
+            self.lr_sch.step()
+            print('Epoch {}/{} ; loss={:.4f}'.format(ep, epochs, loss.item()))
+            
+            if self.use_val:
+                self.net.eval()
+                # Evaluate
+                correct = 0.
+                total = 0.
+                for input, label in self.val_loader:
+                    input, label = input.cuda(), label.cuda()
+                    output = self.net(input)
+                    pred = output.data.max(1)[1]
+                    correct += pred.eq(label.data).cpu().sum()
+                    total += len(label.data)
+                acc = (correct / total).item()
+                print('Accuracy = ', acc)
+                if acc > best_acc:
+                    best_acc = acc
+                    torch.save(self.net.state_dict(), 'Best_model.pth')
+
+    def predict(self, x_test, batch_size=16):
+        self.test_set = MyCIFARDataset(x_test, transform=self.test_trans)
+        self.test_loader = data.DataLoader(
+            dataset=self.test_set,
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False,
+            num_workers=4
+        )
+
+        preds = np.array([])
+        for input in self.test_loader:
+            input = input.cuda()
+            output = self.net(input)
+            pred = output.data.max(1)[1].cpu().numpy()
+            preds = np.concatenate((preds, pred))
+        return preds
+
+
+# In[58]:
+
+
 # Builde model
-model = Sequential() # Sequential groups a linear stack of layers 
-model.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=x_train.shape[1:])) # Add Convolution layers
-model.add(Activation('relu')) # Add Relu activation for non-linearity
-model.add(Conv2D(filters=32, kernel_size=(3, 3))) # Add Convolution layers
-model.add(Activation('relu')) # Add Relu activation for non-linearity
-model.add(MaxPooling2D(pool_size=(4, 4))) # Add Max pooling to lower the sptail dimension
+# Load model made by myself
+#net = ResNet50(num_classes=len(class_index))
 
-model.add(Flatten()) # Flatten the featuremaps
-model.add(Dense(units=512)) # Add dense layer with 512 neurons
-model.add(Activation('relu')) # Add Relu activation for non-linearity
-model.add(Dense(units=num_classes)) # Add final output layer for 10 classes
-model.add(Activation('softmax')) # Add softmax activation 
+# Pretrained model
+#net = torchvision.models.densenet121(pretrained=True)
+net = DenseNet121()
+#net.classifier = nn.Sequential(
+#    nn.Dropout(0.5),
+#    nn.Linear(1024, 512, True),
+#    nn.BatchNorm1d(512),
+#    nn.ReLU(),
+#    nn.Dropout(0.5),
+#    nn.Linear(512, 256, True),
+#    nn.BatchNorm1d(256),
+#    nn.ReLU(),
+#    nn.Linear(256, len(class_index))
+#)
 
-# initiate SGD optimizer
-opt = keras.optimizers.SGD()
-
-# Compile the model with loss function and optimizer
-model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
+model = MyModel(net)
 
 
-batch_size = 32
-epochs = 10
+# In[59]:
+
+
+# Setup some hyperparameters
+batch_size = 128
+epochs = 600
+
 # Fit the data into model
 model.fit(x_train, y_train,
           batch_size=batch_size,
@@ -93,45 +247,26 @@ model.fit(x_train, y_train,
           shuffle=True)
 
 
-# In[10]:
-
-
-y_pred = model.predict(x_test)
-print(y_pred.shape)
-
-
-# In[11]:
-
-
-y_pred[0]
-
-
-# In[12]:
-
-
-np.argmax(y_pred[0])
-
-
-# In[13]:
-
-
-y_pred = np.argmax(y_pred, axis=1)
-
-
 # ## DO NOT MODIFY CODE BELOW!
-# please screen shot your results and post it on your report
+# **Please screen shot your results and post it on your report**
 
-# In[14]:
+# In[ ]:
+
+
+y_pred = your_model.predict(x_test)
+
+
+# In[ ]:
 
 
 assert y_pred.shape == (10000,)
 
 
-# In[15]:
+# In[ ]:
 
 
 y_test = np.load("y_test.npy")
-print("Accuracy of my model on test-set: ", accuracy_score(y_test, y_pred))
+print("Accuracy of my model on test set: ", accuracy_score(y_test, y_pred))
 
 
 # In[ ]:
---
+++ HW5/resnet.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn as nn
+from collections import OrderedDict
+
+
+class BasicBlock(nn.Module):
+    # To sync with bottleneck block
+    expansion = 1
+
+    def __init__(
+        self,  
+        in_features,
+        out_features,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+        norm_layer=None
+    ):
+        
+        super(BasicBlock,self).__init__()
+        
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        
+        self.conv1 = nn.Conv2d(in_features, out_features, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1   = norm_layer(out_features)
+        self.relu1 = nn.ReLU(inplace=True)
+        
+        self.conv2 = nn.Conv2d(out_features, out_features, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2   = norm_layer(out_features)
+        self.relu2 = nn.ReLU(inplace=True)
+        
+        self.downsample = downsample
+        self.identity = None
+        
+        
+    def forward(self,x):
+        # Record identity
+        self.identity = x
+            
+        
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        
+        if self.downsample is not None:
+            self.identity = self.downsample(self.identity)
+        
+        x += self.identity
+        x = self.relu2(x)
+
+        return x
+
+
+class BottleneckBlock(nn.Module):
+    # The number of features of final layer of bottleneck block is 4 times more than the first layer's of it.
+    expansion = 4
+    
+    def __init__(
+        self,
+        in_features,
+        out_features,
+        stride=1,
+        downsample=None,
+        groups=1,
+        base_width=64,
+        dilation=1,
+        norm_layer=None
+    ):   
+        super(BottleneckBlock,self).__init__()
+        
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(out_features * (base_width / 64.)) * groups
+            
+        self.conv1 = nn.Conv2d(in_features, width, kernel_size=1, stride=1, bias=False)
+        self.bn1   = norm_layer(width)
+        self.relu1 = nn.ReLU(inplace=True)
+        
+        self.conv2 = nn.Conv2d(width, width, kernel_size=3, stride=stride, padding=dilation, groups=groups, bias=False, dilation=dilation)
+        self.bn2   = norm_layer(width)
+        self.relu2 = nn.ReLU(inplace=True)
+        
+        self.conv3 = nn.Conv2d(width, out_features*self.expansion, kernel_size=1, stride=1, bias=False)
+        self.bn3   = norm_layer(out_features*self.expansion)
+        self.relu3 = nn.ReLU(inplace=True)
+        
+        
+        self.downsample = downsample
+        self.identity = None
+        self.stride = stride
+             
+    def forward(self,x):
+        # Record identity
+        self.identity = x
+        
+        #print('x.size() =',x.size() , ' ; self.identity.size() = ',self.identity.size())
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        
+        x = self.conv3(x)
+        x = self.bn3(x)
+        
+        if self.downsample is not None:
+            self.identity = self.downsample(self.identity)
+        #print('x.size() =',x.size() , ' ; self.identity.size() = ',self.identity.size())
+        
+        x += self.identity
+        x = self.relu3(x)
+
+        return x
+
+
+class ResNet(nn.Sequential):
+    def __init__(
+        self,
+        block_type=BasicBlock,
+        blocks_per_layer=[2,2,2,2],
+        num_classes=5,
+        zero_init_residual=False,
+        groups=1,
+        width_per_group=64,
+        replace_stride_with_dilation=None,
+        norm_layer=None
+    ):        
+        # Initial in_features for "layer", not for the ResNet model itself
+        self.in_features = 64
+        self.dilation = 1
+        self.groups = groups
+        self.base_width = width_per_group
+        
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+        
+        layer0 = nn.Sequential(OrderedDict([
+            ('conv1'  , nn.Conv2d(3, self.in_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ('bn1'    , nn.BatchNorm2d(self.in_features)),
+            ('relu1'   , nn.ReLU(inplace=True)),
+            ('maxpool', nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        ]))
+        
+        layer1  = self._make_layer(block_type, 64,  blocks_per_layer[0], stride=1)
+        layer2  = self._make_layer(block_type, 128, blocks_per_layer[1], stride=2)
+        layer3  = self._make_layer(block_type, 256, blocks_per_layer[2], stride=2)
+        layer4  = self._make_layer(block_type, 512, blocks_per_layer[3], stride=2)
+        
+        final = nn.Sequential(OrderedDict([
+            ('avgpool', nn.AdaptiveAvgPool2d((1, 1))),
+            ('flatten', nn.Flatten())
+        ]))
+        fc = nn.Sequential(OrderedDict([
+            ('fc1', nn.Linear(512 * block_type.expansion , 64 * block_type.expansion)),
+            ('fc2', nn.Linear(64 * block_type.expansion , num_classes))
+        ]))
+    
+        super(ResNet,self).__init__(OrderedDict([
+            ('layer0' ,layer0),
+            ('layer1' ,layer1),
+            ('layer2' ,layer2),
+            ('layer3' ,layer3),
+            ('layer4' ,layer4),
+            ('final'  ,final),
+            ('fc' , fc)
+        ]))
+        
+    def _make_layer(self, block_type, out_features, num_blocks, stride=1):
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        
+        if stride != 1 or self.in_features != out_features * block_type.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_features, out_features * block_type.expansion, kernel_size=1, stride=stride, bias=False),
+                norm_layer(out_features* block_type.expansion),
+            )
+        
+        layers = []
+        layers.append(block_type(self.in_features, out_features, stride, downsample, self.groups,\
+                            self.base_width, previous_dilation, norm_layer))
+        
+        self.in_features = out_features * block_type.expansion
+        
+        for _ in range(1, num_blocks):
+            layers.append(block_type(self.in_features, out_features, groups=self.groups,\
+                                base_width=self.base_width, dilation=self.dilation,\
+                                norm_layer=norm_layer))
+            
+        return nn.Sequential(*layers)
+
+
+# +
+class ResNet18(ResNet):
+    def __init__(self, num_classes=5):
+        super(ResNet18,self).__init__(BasicBlock, [2, 2, 2, 2], num_classes=num_classes)
+        
+class ResNet34(ResNet):
+    def __init__(self, num_classes=5):
+        super(ResNet34,self).__init__(BasicBlock, [3, 4, 6, 3], num_classes=num_classes)
+        
+class ResNet50(ResNet):
+    def __init__(self, num_classes=5):
+        super(ResNet50,self).__init__(BottleneckBlock, [3, 4, 6, 3], num_classes=num_classes)
+                
+class ResNet101(ResNet):
+    def __init__(self, num_classes=5):
+        super(ResNet101,self).__init__(BottleneckBlock, [3, 4, 23, 3], num_classes=num_classes)
+        
+class ResNet152(ResNet):
+    def __init__(self, num_classes=5):
+        super(ResNet152,self).__init__(BottleneckBlock, [3, 8, 36, 3], num_classes=num_classes)
